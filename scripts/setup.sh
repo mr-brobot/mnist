ec5ecd161a9f9c4a30a8beb5d98669f383f3b7e9
Josh Wiley <josh@cloudbend.dev>
2025-02-10 13:59:32 -0800

    feat: analysis & cnn notebooks

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,15 +7,8 @@ pip install notebook
 pip install ipywidgets
 pip install matplotlib
 
-# https://github.com/evcxr/evcxr/blob/main/evcxr_jupyter/README.md
-cargo install --locked evcxr_jupyter
-evcxr_jupyter --install
-
 # https://pytorch.org/
-pip install torch torchvision torchaudio
-
-# huggingface
-pip install datasets
+pip install torch torchvision
 
 # wandb
 pip install wandb
